e547e3aca0dafcb624ccf0dd53c3911074d635c2
AnthonyLam <alam6@stuy.edu>
2018-11-16 12:07:29 -0500

    finished

--- fork.c
+++ fork.c
@@ -2,24 +2,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-  int parent_pid = getpid();
-  int child = fork();
-  int child2;
-  int status;
-  printf("Before fork");
-  if (child == 0){
-    sleep(5);
-  }
-  else{
-    child2 = fork();
-    if (child2 == 0){
-    }
-    else{
-      wait(status);
-      printf("The time was %d", WEXITSTATUS(status));
-      return 0;
-    }
-  return 0;
+	srand(time(NULL));
+	printf("Before fork, parent pid %d\n", getpid());
+	int child = fork();
+	int child2;
+	int status;
+	if (!child){
+		int secs = rand()%15+5;
+		sleep(secs);
+		printf("Child Process finished, pid was %d\n", getpid());
+		return secs;
+	}
+	else{
+		child2 = fork();
+		if (!child2){
+			int secs = rand()%15+5;
+			sleep(secs);
+			printf("Child Process finished, pid was %d\n", getpid());
+			return secs;
+		}
+		else{
+			int child_pid = wait(&status);
+			printf("The pid of child was %d\n", child_pid);
+			printf("The time was %d\n", WEXITSTATUS(status));
+			return 0;
+		}
+		return 0;
+	}
 }
